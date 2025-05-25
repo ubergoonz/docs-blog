a08eb523f6449fd1807865dc3f3f2613f1784b26
Leslie Wang <ubergoonz@gmail.com>
2025-05-25 22:49:13 +0800

    initial check in to test

--- overrides/hooks/socialmedia.py
+++ overrides/hooks/socialmedia.py
@@ -1,49 +1,58 @@
-from textwrap import dedent
 import urllib.parse
-import re
 
 x_intent = "https://x.com/intent/tweet"
 fb_sharer = "https://www.facebook.com/sharer/sharer.php"
-linkedin_sharer = "https://www.linkedin.com/sharing/share-offsite/"  # LinkedIn share URL
-wa_sharer = "https://wa.me/?text="  # WhatsApp share URL
-tg_sharer = "https://t.me/share/url?url="  # Telegram share URL
-notion_url = "https://www.notion.so/"
-onenote_url = "https://www.onenote.com/"
-wechat_url = "https://web.wechat.com/"
-xiaohongshu_url = "https://www.xiaohongshu.com/"
-line_sharer = "https://social-plugins.line.me/lineit/share?url="  # LINE share URL
-include = re.compile(r"blog/[1-9].*")
+linkedin_sharer = "https://www.linkedin.com/sharing/share-offsite/"
+
+x_owner = "@ubergoonz"
+fb_owner = "@ubergoonz"
+li_owner = "@lesliewang"
 
 def on_page_markdown(markdown, **kwargs):
     page = kwargs['page']
     config = kwargs['config']
-    if page.meta.get('template') != 'blog-post.html': # Only apply the social media tags to blog posts
-    #if not include.match(page.url):
+    if page.meta.get('template') != 'blog-post.html':
         return markdown
 
-    page_url = config.site_url+page.url
-    page_title = urllib.parse.quote(page.title+'\n')
+    page_url = config.site_url + page.url
+    page_title = page.title
 
+    x_message = f"Check out this awesome blog post by {x_owner}: {page_title} {page_url}"
+    fb_message = f"Check out this awesome blog post by {fb_owner}: {page_title} {page_url}"
+    li_message = f"Check out this awesome blog post by {li_owner}: {page_title} {page_url}"
 
-    return markdown + dedent(f"""
-    <div style="text-align: center;" markdown="1">
-    
-    ---
+    encoded_x_message = urllib.parse.quote(x_message)
+    encoded_fb_message = urllib.parse.quote(fb_message)
+    encoded_li_message = urllib.parse.quote(li_message)
+    encoded_url = urllib.parse.quote(page_url)
 
-    :octicons-share-android-16: **Share on Socials** 
-    [:simple-x:]({x_intent}?text={page_title}&url={page_url}){{ target="_blank" title="Share on X" }}
-    [:simple-facebook:]({fb_sharer}?u={page_url}){{ target="_blank" title="Share on Facebook" }}
-    [:material-linkedin:]({linkedin_sharer}?url={page_url}){{ target="_blank" title="Share on LinkedIn" }}
-    [:simple-line:]({line_sharer}{urllib.parse.quote(page_url)}){{ target="_blank" title="Share on LINE" }}
-    [:simple-whatsapp:]({wa_sharer}{urllib.parse.quote(f"{page.title}\n{page_url}")}){{ target="_blank" title="Share on WhatsApp" }}
-    [:simple-telegram:]({tg_sharer}{urllib.parse.quote(page_url)}&text={urllib.parse.quote(page.title)}){{ target="_blank" title="Share on Telegram" }}
-    [:simple-wechat:]({wechat_url}){{ target="_blank" title="Copy post URL for WeChat" }}
-    [:simple-notion:]({notion_url}){{ target="_blank" title="Copy post URL for Notion" }}
-    [:material-microsoft-onenote:]({onenote_url}){{ target="_blank" title="Copy post URL for OneNote" }}
-    [:simple-xiaohongshu:]({xiaohongshu_url}){{ target="_blank" title="Copy post URL for Xiaohongshu" }}
+    share_md = f"""
+---
+**Share this post**
 
-    :octicons-share-android-16: **Share on others**
-    [Copy Link :material-share-all:](javascript:void(0); "Copy post URL")
-    [Add to Bookmark :material-bookmark:](javascript:void(0); "Add to Bookmark")
-    </div>
-    """)
+<a href="{x_intent}?text={encoded_x_message}" target="_blank" class="x-share" data-message="{x_message}">
+:simple-x:
+</a>
+<a href="{fb_sharer}?u={encoded_url}" target="_blank" class="fb-share" data-message="{fb_message}">
+:simple-facebook:
+</a>
+<a href="{linkedin_sharer}?url={encoded_url}&summary={encoded_li_message}" target="_blank" class="li-share" data-message="{li_message}">
+:material-linkedin:
+</a>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {{
+  var shares = document.querySelectorAll('a.fb-share, a.li-share');
+  shares.forEach(function(link) {{
+    link.addEventListener('click', function(e) {{
+      navigator.clipboard.writeText(link.getAttribute('data-message'));
+      setTimeout(function() {{
+        alert('Message copied! Paste it in the share dialog.');
+      }}, 100);
+    }});
+  }});
+}});
+</script>
+---
+"""
+    return markdown + share_md
